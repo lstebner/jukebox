2f3ab2a80383ac6babc206d3542325b8aa1fa51c
luke <luke.stebner@gmail.com>
2017-10-14 10:16:46 -0700

    basic setup

---
+++ entry.js
@@ -0,0 +1,8 @@
+require("./style.css");
+const Jukebox = require("./jukebox.jsx");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Jukebox/>, document.getElementById("root"));
+
---
+++ jukebox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Jukebox extends React.Component {
+  render() {
+    return (
+      <div className="jukebox">
+        Hello, jukebox.
+      </div>
+    );
+  }
+}
+
+module.exports = Jukebox;
