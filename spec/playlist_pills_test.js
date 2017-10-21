3caf0d0b5045ec6a1ac8f64aadbab80b3884afd0
luke <luke.stebner@gmail.com>
2017-10-21 09:58:21 -0700

    adding PlaylistPills test

---
+++ spec/playlist_pills_test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import {mount, shallow, render} from 'enzyme';
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import spies from 'chai-spies-next';
+
+configure({ adapter: new Adapter() });
+chai.use(chaiEnzyme());
+chai.use(spies);
+
+const {expect} = chai;
+
+import {PlaylistPills} from './../jukebox-components';
+
+describe("<PlaylistPills />", function() {
+  it("can render", function() {
+    const wrapper = shallow(<PlaylistPills />);
+    expect(wrapper).to.have.className("playlist-pills");
+  });
+
+  it("renders a .playlist_pill for each playlist", function() {
+    const playlists = [
+      { name: "" },
+      { name: "" },
+      { name: "" },
+    ];
+    const wrapper = shallow(<PlaylistPills playlists={playlists} />);
+    expect(wrapper).to.have.exactly(playlists.length).descendants(".playlist_pill");
+  });
+
+  it("marks the selected playlist with .is_selected", function() {
+    const playlists = [
+      { name: "" },
+      { name: "" },
+      { name: "", is_selected: true },
+    ];
+    const wrapper = shallow(<PlaylistPills playlists={playlists} />);
+    expect(wrapper).to.have.exactly(1).descendants(".playlist_pill.is_selected");
+  });
+});
+
+
+
