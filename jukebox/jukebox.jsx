cc54428b17e629180333acc2617d140aea365f5c
luke <luke.stebner@gmail.com>
2017-10-21 20:57:59 -0700

    adding jukebox data as a js file and parsing it in Jukebox

--- jukebox/jukebox.jsx
+++ jukebox/jukebox.jsx
@@ -3,6 +3,7 @@ require("./jukebox.less");
 import React from 'react';
 import types from 'prop-types';
 import {Header, PlaylistPills, BeatsList, NowPlayingFooter} from './jukebox-components.jsx';
+import jukeboxdata from './data.js';
 
 class Jukebox extends React.Component {
   static propTypes = {
@@ -16,10 +17,20 @@ class Jukebox extends React.Component {
   constructor(props) {
     super(props);
 
+    this.playlist_keys = [];
+    this.playlists = [];
+    this.songs = [];
+
     this.state = {
       is_playing: false,
       now_playing_track_id: -1,
+      songs: [],
+      playlists: [],
     }
+  }
+
+  componentDidMount() {
+    this.parse_data(jukeboxdata);
   }
 
   render() {
@@ -39,6 +50,47 @@ class Jukebox extends React.Component {
         <NowPlayingFooter />
       );
     }
+  }
+
+  parse_data(data) {
+    this.playlist_keys = [];
+    this.playlists = [];
+    this.songs = [];
+
+    //for (let playlist in Object.keys(data.songs)) {
+    for (let playlist in data.songs) {
+      this.add_playlist(playlist);
+      for (let i = 0; i < data.songs[playlist].length; i++) {
+        this.add_song(data.songs[playlist][i], playlist);
+      }
+    }
+
+    this.setState({
+      playlists: this.playlists,
+      beats: this.songs,
+    });
+  }
+
+  add_playlist(playlist) {
+    if (this.playlist_keys.indexOf(playlist) < 0) {
+      this.playlist_keys.push(playlist);
+      this.playlists.push({
+        key: playlist,
+        name: playlist.replace(/_/g, " "),
+      });
+    }
+  }
+
+  add_song(song, playlist) {
+    if (typeof song.tags === "undefined") {
+      song.tags = [];
+    }
+
+    song.name = song.name || song.filepath;
+    song.filename = `${song.key}/${song.filepath}`;
+    song.tags.push(playlist);
+
+    this.songs.push(song);
   }
 }
 
