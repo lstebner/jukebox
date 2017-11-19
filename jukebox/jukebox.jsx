882be077da307295f85a8f586e76d9c6e709447b
luke <luke.stebner@gmail.com>
2017-11-19 14:39:48 -0800

    human linting + adding more specs for jukebox

--- jukebox/jukebox.jsx
+++ jukebox/jukebox.jsx
@@ -1,8 +1,8 @@
-require("./jukebox.less");
+require("./jukebox.less")
 
-import React from 'react';
-import types from 'prop-types';
-import {Header, PlaylistPills, BeatsList, NowPlayingFooter} from './jukebox-components.jsx';
+import React from 'react'
+import types from 'prop-types'
+import {Header, PlaylistPills, BeatsList, NowPlayingFooter} from './jukebox-components.jsx'
 
 class Jukebox extends React.Component {
   static propTypes = {
@@ -14,11 +14,11 @@ class Jukebox extends React.Component {
   }
 
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.playlist_keys = [];
-    this.playlists = [];
-    this.songs = [];
+    this.playlist_keys = []
+    this.playlists = []
+    this.songs = []
 
     this.state = {
       is_playing: false,
@@ -29,78 +29,92 @@ class Jukebox extends React.Component {
   }
 
   componentDidMount() {
-    this.parse_data(this.props.jukeboxdata);
-    this.load_material_icons();
+    this.parse_data(this.props.jukeboxdata)
+    this.load_material_icons()
   }
 
   load_material_icons() {
-    const tag = document.createElement("link");
-    tag.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
-    tag.rel = "stylesheet";
-    const head = document.getElementsByTagName("head")[0];
-    head.appendChild(tag);
+    const tag = document.createElement("link")
+    tag.href = "https://fonts.googleapis.com/icon?family=Material+Icons"
+    tag.rel = "stylesheet"
+    const head = document.getElementsByTagName("head")[0]
+    head.appendChild(tag)
   }
 
   render() {
     return (
       <div className="jukebox">
+        {this.render_audio_tag()}
         <Header />
         <PlaylistPills playlists={this.state.playlists} />
         <BeatsList beats={this.state.songs} />
         {this.render_now_playing()}
       </div>
-    );
+    )
+  }
+
+  render_audio_tag() {
+    return (
+      <audio controls preload="auto" autobuffer="true">
+        <source src={this.state.current_track_filename} type={`audio/${this.state.current_track_filetype}`} />
+      </audio>
+    )
   }
 
   render_now_playing() {
     if (this.state.is_playing) {
       return (
         <NowPlayingFooter />
-      );
+      )
     }
   }
 
   parse_data(data) {
-    this.playlist_keys = [];
-    this.playlists = [];
-    this.songs = [];
+    this.playlist_keys = []
+    this.playlists = []
+    this.songs = []
 
     //for (let playlist in Object.keys(data.songs)) {
     for (let playlist in data.songs) {
-      this.add_playlist(playlist);
+      this.add_playlist(playlist)
       for (let i = 0; i < data.songs[playlist].length; i++) {
-        this.add_song(data.songs[playlist][i], playlist);
+        this.add_song(data.songs[playlist][i], playlist)
       }
     }
 
     this.setState({
       playlists: this.playlists,
       songs: this.songs,
-    });
+    })
   }
 
-  add_playlist(playlist) {
+  add_playlist(playlist, name) {
     if (this.playlist_keys.indexOf(playlist) < 0) {
-      this.playlist_keys.push(playlist);
+      this.playlist_keys.push(playlist)
       this.playlists.push({
         key: playlist,
-        name: playlist.replace(/_/g, " "),
-      });
+        name: name || playlist.replace(/_/g, " "),
+      })
+
+      return true
+    }
+    else {
+      return false
     }
   }
 
   add_song(song, playlist) {
     if (typeof song.tags === "undefined") {
-      song.tags = [];
+      song.tags = []
     }
 
-    song.name = song.name || song.filepath;
-    song.filename = `${song.key}/${song.filepath}`;
-    song.tags.push(playlist);
-    song.is_playing = false;
+    song.name = song.name || song.filepath
+    song.filename = `${song.key}/${song.filepath}`
+    song.tags.push(playlist)
+    song.is_playing = false
 
-    this.songs.push(song);
+    this.songs.push(song)
   }
 }
 
-module.exports = Jukebox;
+module.exports = Jukebox
--- spec/jukebox_test.js
+++ spec/jukebox_test.js
@@ -1,44 +1,137 @@
-import React from 'react';
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-import {mount, shallow, render} from 'enzyme';
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from 'react'
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import {mount, shallow, render} from 'enzyme'
+import {configure} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-configure({ adapter: new Adapter() });
-chai.use(chaiEnzyme());
+configure({ adapter: new Adapter() })
+chai.use(chaiEnzyme())
 
-const {expect} = chai;
+const {expect} = chai
 
-import Jukebox from './../jukebox/jukebox.jsx';
-import {Header, BeatsList, PlaylistPills, NowPlayingFooter} from './../jukebox/jukebox-components.jsx';
+import Jukebox from './../jukebox/jukebox.jsx'
+import {Header, BeatsList, PlaylistPills, NowPlayingFooter} from './../jukebox/jukebox-components.jsx'
 
 describe("<Jukebox />", function() {
-  it("can render", function() {
-    const wrapper = shallow(<Jukebox />);
-    expect(wrapper).to.have.className("jukebox");
-  });
+  const find_playlist = function(name, playlists) {
+    const find_playlist_fn = function(item) {
+      return item.key === name
+    }
 
-  it("contains a <Header>", function() {
-    const wrapper = shallow(<Jukebox />);
-    expect(wrapper).to.contain(<Header/>);
-  });
+    return playlists.find(find_playlist_fn)
+  }
 
-  it("contains a <BeatsList>", function() {
-    const wrapper = shallow(<Jukebox />);
-    expect(wrapper).to.contain(<BeatsList/>);
-  });
+  describe("#render", function() {
+    it("renders", function() {
+      const wrapper = shallow(<Jukebox />)
+      expect(wrapper).to.have.className("jukebox")
+    })
 
-  it("contains a <NowPlayingFooter> when state.is_playing", function() {
-    const wrapper = mount(<Jukebox />);
-    wrapper.setState({ is_playing: true });
-    expect(wrapper).to.contain(<NowPlayingFooter/>);
-  });
+    it("contains <Header>", function() {
+      const wrapper = shallow(<Jukebox />)
+      expect(wrapper).to.contain(<Header/>)
+    })
 
-  it("contains a <PlaylistPills>", function() {
-    const wrapper = shallow(<Jukebox />);
-    expect(wrapper).to.contain(<PlaylistPills />);
-  });
-});
+    it("contains <BeatsList>", function() {
+      const wrapper = shallow(<Jukebox />)
+      expect(wrapper).to.contain(<BeatsList/>)
+    })
+
+    it("contains <NowPlayingFooter> when state.is_playing", function() {
+      const wrapper = mount(<Jukebox />)
+      wrapper.setState({ is_playing: true })
+      expect(wrapper).to.contain(<NowPlayingFooter/>)
+    })
+
+    it("contains <PlaylistPills>", function() {
+      const wrapper = shallow(<Jukebox />)
+      expect(wrapper).to.contain(<PlaylistPills />)
+    })
+
+    it("contains an <audio> tag", function() {
+      const wrapper = shallow(<Jukebox />)
+      expect(wrapper).to.have.exactly(1).descendants("audio")
+    })
+  })
+
+  it("calls #load_material_icons on mount", function() {
+    class TestJukebox extends Jukebox {
+      load_material_icons = chai.spy()
+    }
+
+    const wrapper = mount(<TestJukebox/>)
+    expect(wrapper.instance().load_material_icons).to.have.been.called
+  })
+
+  describe("#add_playlist", function() {
+    it("returns true when a playlist is added", function() {
+      const wrapper = mount(<Jukebox/>)
+      const test_playlist = "greatest_hits"
+      const result = wrapper.instance().add_playlist(test_playlist)
+      expect(result).to.be.true
+    })
+
+    it("adds a playlist to playlist_keys", function() {
+      const wrapper = mount(<Jukebox/>)
+      const test_playlist = "greatest_hits"
+      wrapper.instance().add_playlist(test_playlist)
+      expect(wrapper.instance().playlist_keys.includes(test_playlist)).to.be.true
+    })
+
+    it("adds a playlist to playlists", function() {
+      const wrapper = mount(<Jukebox/>)
+      const test_playlist = "greatest_hits"
+      wrapper.instance().add_playlist(test_playlist)
+      const playlist = find_playlist(test_playlist, wrapper.instance().playlists)
+      expect(typeof playlist).to.equal("object", "test_playlist not found in playlists")
+    })
+
+    it("will not return false when a playlist is not added", function() {
+      const wrapper = mount(<Jukebox/>)
+      const test_playlist = "greatest_hits"
+      wrapper.instance().add_playlist(test_playlist)
+      const result = wrapper.instance().add_playlist(test_playlist)
+      expect(result).to.be.false
+    })
+
+    it("will not add a playlist twice", function() {
+      const wrapper = mount(<Jukebox/>)
+      const test_playlist = "greatest_hits"
+      for (let i = 0; i < 5; i++) {
+        wrapper.instance().add_playlist(test_playlist)
+      }
+      const result = wrapper.instance().add_playlist(test_playlist)
+      let num_times = 0
+      for (let key of wrapper.instance().playlist_keys) {
+        if (key === test_playlist) {
+          num_times++
+        }
+      }
+      expect(num_times).to.equal(1)
+    })
+
+    it("will take the playlist name as an optional parameter", function() {
+      const wrapper = mount(<Jukebox/>)
+      const test_playlist = "greatest_hits"
+      const test_name = "Great Songs"
+      wrapper.instance().add_playlist(test_playlist, test_name)
+      const playlist = find_playlist(test_playlist, wrapper.instance().playlists)
+      expect(playlist.name).to.equal(test_name)
+    })
+
+    it("will auto generate the name from the key if not given a name", function() {
+      const wrapper = mount(<Jukebox/>)
+      const test_playlist = "greatest_hits"
+      const expected_name = "greatest hits"
+      wrapper.instance().add_playlist(test_playlist)
+      const playlist = find_playlist(test_playlist, wrapper.instance().playlists)
+      expect(playlist.name).to.equal(expected_name)
+    })
+  })
+
+  describe("#add_song", function() {
+  })
+})
 
 
